92a86baad0e85dc719d169057ddf2990c205e993
bxvail <BryantVail@gmail.com>
2018-03-05 00:25:58 -0500

    Notes added for errors

--- main.js
+++ main.js
@@ -1,12 +1,63 @@
-var color = 'blue';
+/*
+**  Errors:
+**      -Syntax
+**      -Runtime
+**      -Logical
+**
+**  Methods for Exception Handling
+**      -Try..catch..finally statment
+**      -Throw statement
+**      -The 'onerror()' method. (on Error)
 
-function printColor(){
-    if(arguments[0]){
-        color = 'purple';
-    }
-    console.log(color);
+    'try' block executes code that is subject to break
+        -MUST BE followed by either:
+            1. exactly 1 'catch' block|| exactly 1 'funally' block
+            2. Exception is passed to the 'catch' block
+        'catch' block 
+            -executes code that should only be ran when the exception is caught.
+        'finally' block: 
+            -optional
+            -will always execute.
+        -note:
+            -syntax errors will not be caught
+            -'runtime' && 'programmer generated' exceptions caught.
 
-}
-printColor();
-printColor(color);
+    onerror Method:
+        -event handler fired when an exception occurs
+        -declared as a function throughout the code
+        -3 pieces of info:
+            -error message
+            -File where the error occurred
+            -Line Number in the code
+        -info is usually manipulated to display as needed. 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
