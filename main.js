2ad608b6cd44dd81f0bd0d64c074257084d8ab2a
bxvail <BryantVail@gmail.com>
2018-03-06 02:01:36 -0500

    var orange = new Order('Orange', 4.25);

--- main.js
+++ main.js
@@ -1,12 +1,75 @@
-var color = 'blue';
+/*
+    module:
+        Creating Objects
+        Dot & Bracket notation
+            Bracket:
+                -allows property name through other variable names
+                -
+            Dot:
+                -Period between the object name & property
+        Constructors
+        Prototypes
+        Inheritance
+        Encapsulation
 
-function printColor(){
-    if(arguments[0]){
-        color = 'purple';
-    }
-    console.log(color);
+    How to Create an Object
+        1. Object Literals
+        2. Object constructor
+*/
 
-}
-printColor();
-printColor(color);
+//creating an object
+
+function Order(user, total){
+    //log creation of object
+    console.log('OrderCreated');
+
+    this.user   = user;
+    this.total  = total;
+    this.type   = function(){
+        console.log('Hi, I\'m a fruit!');
+    };//end this.type  = function(){
+}//end function Order(user, total){
+
+var orange = new Order('Orange', 4.25);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
